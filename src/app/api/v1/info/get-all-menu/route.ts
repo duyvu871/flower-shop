a428751716abd05822c4cfe61420177dcb2dc7a5
duyvu871 <zoocp123@gmail.com>
2024-03-22 15:29:14 +0700

    api_feature/cache_all_menu: revalidate api route

--- src/app/api/v1/info/get-all-menu/route.ts
+++ src/app/api/v1/info/get-all-menu/route.ts
@@ -1,10 +1,11 @@
 import { NextResponse, NextRequest } from "next/server";
+import { revalidateTag } from 'next/cache';
 // import {ObjectId} from "mongodb";
 import {getAllMenuList} from "@/lib/order";
 // import {useSearchParams} from "next/navigation";
 // import {timeOrder} from "types/order";
 // import DBConfigs from "@/configs/database.config";
-
+export const revalidate = 360
 export async function GET(req: NextRequest) {
     // const searchParams = new URL(req.url).searchParams;
     // const time = searchParams.get("time") as timeOrder;
@@ -12,7 +13,7 @@ export async function GET(req: NextRequest) {
     // const limit = searchParams.get("limit") || String(DBConfigs.perPage);
     // console.log(time);
     const allMenuList = await getAllMenuList();
-
+    revalidateTag('allMenuList');
     if (!allMenuList) {
         return NextResponse.json({error: "Xảy ra lỗi gì đó ở menu session"}, {status: 404});
     }
