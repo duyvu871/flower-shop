b096e854f0b8c1777667896ee19ead84165250d7
duyvu871 <zoocp123@gmail.com>
2024-04-30 18:08:53 +0700

    change the way to process server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	apps: [
 		{
 			name: 'app1',
-			script: 'set DEV_HOSTNAME=14.225.218.92&&set PORT=3000&&node server.js',
+			script: 'set DEV_HOSTNAME=14.225.218.92&&set PORT=3000&&npx next start',
 			watch: '.',
 			env_production: {
 				NODE_ENV: 'production',
